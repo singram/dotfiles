afeb5e93691e557a540ea5e9de7f05c3269ba8ef
Stuart Ingram <singram@medsagetechnologies.com>
2011-03-05 15:53:28 -0500

    Simplified initial rvm setup & added rvmrc preferences

--- install_rvm.sh
+++ install_rvm.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 source ~/.bashrc
 if [[ -z `which rvm` ]]; then
+  SCRIPTLOCATION=`dirname "$0"`
   bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
   cp ~/.bashrc ~/.bashrc_pre_rvm
-  SEARCH='[ -z "$PS1" ] && return'
-  REPLACE='if [[ -n "$PS1" ]]; then'
-  sed -i "s|$SEARCH|$REPLACE|g" ~/.bashrc
-  ~/.bashrc <<EOF
- if [[ -s $HOME/.rvm/scripts/rvm ]] ; then source $HOME/.rvm/scripts/rvm ; fi
-
-fi
-EOF
+  cp $SCRIPTLOCATION/bashrc ~/.bashrc
   source ~/.bashrc
   rvm install ree-1.8.7-2010.02
   rvm --default ree-1.8.7-2010.02
