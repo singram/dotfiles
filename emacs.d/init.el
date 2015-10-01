3999834f803d71b6ba8b5e03eb0d3fead52e3047
Stuart Ingram <ingrams2@upmc.com>
2015-10-01 11:10:35 -0400

    Emacs update for 24.4 and change in starter-kit approach

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,14 +1,13 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+
 (package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-bindings starter-kit-ruby color-theme ruby-block feature-mode rspec-mode yaml-mode )
-  "A list of packages to ensure are installed at launch.")
+(defvar my-packages '(better-defaults paredit idle-highlight-mode ido-ubiquitous color-theme ruby-block feature-mode rspec-mode yaml-mode starter-kit-bindings starter-kit-ruby
+                                      find-file-in-project magit smex scpaste))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
