559d93b2767bce3137751b1476b782852e86ed5d
Stuart Ingram <ingrams2@upmc.com>
2021-06-01 16:21:16 -0400

    Added package installer

---
+++ install_tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Useful packages
+apt-get install net-tools clusterssh curl dstat htop iftop nmap sshfs sysstat tmux
+
+# Build tools
+#apt-get install dconf-tools build-essential
+
+# Libraries
+#apt-get install libaio1 libffi-dev libgdbm-dev libgtk-3-dev libgtk2.0-dev libjpeg62 libreadline6-dev libsqlite3-dev libssl-dev libxml2-dev libxslt1-dev libyaml-dev zlib1g-dev
